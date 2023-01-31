8790a9bb02b461c2fd31ffa106166c8c96f5fb59
FilippoToffoletto <filippotoffoletto@hotmail.it>
2023-01-31 16:41:12 +0100

    [print]api with vue

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,10 +1,40 @@
 <script>
-export default {
 
+import axios from 'axios';
+
+export default {
+    name:'App',
+    data(){
+        return {
+            baseUrl: 'http://127.0.0.1:8000/api/',
+            projects : []
+        }
+    },
+    methods:{
+        getApi(){
+            axios.get(this.baseUrl + 'projects')
+            .then(result => {
+                this.projects = result.data.projects;
+                console.log(this.projects);
+            })
+        }
+    },
+    mounted(){
+        this.getApi();
+    }
 }
 </script>
 
 <template>
+
+    <div class="container">
+        <h1>Bentornato Vue</h1>
+
+        <div v-for="project in  projects" :key="project.id" class="post-box">
+            <h3>{{project.name}}</h3>
+            <p>{{ project.summary }}}</p>
+        </div>
+    </div>
 
 </template>
 
---
+++ resources/js/appVue.js
@@ -0,0 +1,7 @@
+//importare vue
+
+import { createApp } from "vue";
+
+import App from './App.vue';
+
+createApp(App).mount('#app');
